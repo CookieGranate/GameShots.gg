bbcb075ba4d911cf0e8e877922362dd958c9c5cd
CookieGranate <cookiesooslol@gmail.com>
2023-01-26 13:34:39 +0100

    Log in page fertig

--- src/components/login/Oauth.jsx
+++ src/components/login/Oauth.jsx
@@ -1,17 +1,29 @@
+import facebooklogo from "./img/facebook_icon.png";
+import googlelogo from "./img/google_icon.png"
+
 const Oauth = () => {
     return (
         <div id="oauth">
-            <ul>
-                <li>
-                    <button id="googleauth" onClick={() => window.location.href = '#'}>Log in with Google</button>
-                </li>
+            <div id="authbtns">
+                <ul>
+                    <li>
+                        <button id="googleauth" className="authbtn" onClick={() => window.location.href = '#'}>
+                            <img id="googlelogo" className="authlogo" src={googlelogo} alt="Google Logo" />
+                            <p>Log in with Google</p> 
+                        </button>
+                    </li>
 
-                <li>
-                    <button id="fbauth" onClick={() => window.location.href = '#'}>Log in with Facebook</button>
-                </li>
-            </ul>
+                    <li>
+                        <button id="fbauth" className="authbtn" onClick={() => window.location.href = '#'}>
+                            <img id="fblogo"className="authlogo" src={facebooklogo} alt="Facebook Logo" />
+                            <p>Log in with Facebook</p>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+            
         </div>
     )
 }
 
-export default Oauth
+export default Oauth;
